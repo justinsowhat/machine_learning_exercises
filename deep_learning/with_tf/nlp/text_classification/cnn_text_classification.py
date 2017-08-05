b67011dd506d52b5d080a935a00aecced55dc563
Justin So <justinsu123@gmail.com>
2017-08-05 13:01:38 -0400

    completed CNN text classification stuff...

--- deep_learning/with_tf/nlp/text_classification/cnn_text_classification.py
+++ deep_learning/with_tf/nlp/text_classification/cnn_text_classification.py
@@ -1,15 +1,14 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.contrib import learn
-
-from deep_learning.with_tf.nlp.text_classification import data_helpers
+import data_helpers
 
 
 class CNN(object):
     def __init__(self, seq_len, num_classes, vocab_size, embedding_size, filter_sizes, num_filters, l2_reg=0.0):
 
         self.X = tf.placeholder(tf.int32, [None, seq_len])
-        self.y = tf.placeholder(tf.float, [None, num_classes])
+        self.y = tf.placeholder(tf.float32, [None, num_classes])
         self.dropout_keep_prob = tf.placeholder(tf.float32)
 
         l2_loss = tf.constant(0.0)
@@ -30,7 +29,7 @@ class CNN(object):
                 padding="VALID")
             h = tf.nn.relu(tf.nn.bias_add(conv, b))
             pooled = tf.nn.max_pool(h,
-                                    ksize=[1, sequence_length - filter_size + 1, 1, 1],
+                                    ksize=[1, seq_len - filter_size + 1, 1, 1],
                                     strides=[1, 1, 1, 1],
                                     padding='VALID')
             pooled_outputs.append(pooled)
@@ -70,9 +69,11 @@ shuffle_indices = np.random.permutation(np.arange(len(y)))
 x_shuffled = x[shuffle_indices]
 y_shuffled = y[shuffle_indices]
 
-dev_index = -1 * int(0.1 * float(len(y)))
-x_train, x_dev = x_shuffled[:dev_index], x_shuffled[dev_index:]
-y_train, y_dev = y_shuffled[:dev_index], y_shuffled[dev_index:]
+dev_index = 1 * int(0.8 * float(len(y)))
+test_index = 1 * int(0.9 * float(len(y)))
+
+x_train, x_dev, x_test = x_shuffled[:dev_index], x_shuffled[dev_index:test_index], x_shuffled[test_index:]
+y_train, y_dev, y_test = y_shuffled[:dev_index], y_shuffled[dev_index:test_index], y_shuffled[test_index:]
 
 
 g = tf.Graph()
@@ -90,17 +91,45 @@ with g.as_default():
             vocab_size=len(vocab_processor.vocabulary_),
             embedding_size=100,
             filter_sizes=[3, 4, 5],
+            num_filters=128,
             l2_reg=0.0
         )
 
-    global_step = tf.Variable(0, trainable=False)
-    optimizer = tf.train.AdamOptimizer(1e-3)
-    grads_and_vars = optimizer.compute_gradients(cnn.loss)
-    train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+        global_step = tf.Variable(0, trainable=False)
+        optimizer = tf.train.AdamOptimizer(1e-3)
+        grads_and_vars = optimizer.compute_gradients(cnn.loss)
+        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
 
-    session.run(tf.global_variables_initializer())
+        session.run(tf.global_variables_initializer())
 
-    # TODO
-    # feed in data in train and dev steps...
-    # gradient descent...
-    # evaluation...
+        def train(x_batch, y_batch):
+            feed_dict = {cnn.X: x_batch, cnn.y: y_batch, cnn.dropout_keep_prob: 0.5}
+            step, loss, accuracy = session.run([global_step, cnn.loss, cnn.accuracy] ,feed_dict)
+            print("step: {step}, loss: {loss:.5f}, accuracy: {accuracy:.5f}".format(step=step,
+                                                                                    loss=loss,
+                                                                                    accuracy=accuracy))
+        batches = data_helpers.batch_iter(list(zip(x_train, y_train)), batch_size=64, num_epochs=100)
+
+        for batch in batches:
+            x_batch, y_batch = zip(*batch)
+            print("Training Step")
+            train(x_batch, y_batch)
+            current_step = tf.train.global_step(session, global_step)
+            if current_step % 1000 == 0:
+                print("=========================")
+                print("Evaluation on the dev set:")
+                train(x_dev, y_dev)
+                print("=========================")
+
+        # evaluation on the test set
+        predictions = []
+        batches = data_helpers.batch_iter(list(x_test), 64, 1, shuffle=False)
+        for x_test_batch in batches:
+            feed_dict = {cnn.X: x_test_batch, cnn.dropout_keep_prob: 1.0}
+            batch_predictions = session.run(cnn.predictions, feed_dict)
+            predictions = np.concatenate([predictions, batch_predictions])
+
+        correct_predictions = float(sum(predictions))
+        print("Test set size: %s" % len(y_test))
+        print("Accuracy on the test set: {:.3f}".format(correct_predictions / len(y_test)))
+
